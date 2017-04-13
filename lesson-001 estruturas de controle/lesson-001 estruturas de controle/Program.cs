f07bce5f35b4747ab42b55f363e796c1690f7823
leoallvez <leoallvez@hotmail.com>
2017-04-13 00:34:28 -0300

    first commit

---
+++ lesson-001 estruturas de controle/lesson-001 estruturas de controle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_001_estruturas_de_controle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 200.00;
+            double valorSaque = 100.00;
+
+            if(saldo >= valorSaque)
+            {
+                Console.Write("Saque realizado com sucesso");
+                Console.ReadLine();
+            } else
+            {
+                Console.Write("Saldo insuficiente");
+                Console.ReadLine();
+            }
+        }
+    }
+}
